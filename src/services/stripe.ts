5093426796e700126bf901357906b0ce32cf04b1
gabrielsb18 <gbmodelator@gmail.com>
2024-05-29 02:16:32 -0300

    Redirecionando para o Stripe

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,4 @@
 import Stripe from "stripe";
-import {version} from "../../package.json"
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,7 +9,7 @@ export const stripe = new Stripe(
     apiVersion: '2020-08-27',
     appInfo: {
       name: "igNews",
-      version
+      version: process.env.npm_package_version
     },
   }
 )
